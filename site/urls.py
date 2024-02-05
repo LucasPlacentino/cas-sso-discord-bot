ede04fa85f7d7cebdeedab414211109bc83dc41f
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-02-05 21:37:44 +0100

    woops

---
+++ site/urls.py
@@ -0,0 +1,27 @@
+"""site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import django_cas_ng.views
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('ping', views.ping, name='ping'),
+    path('admin/', admin.site.urls),
+    path('accounts/login', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
+    path('accounts/logout', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
+]
---
+++ site/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpRequest, HttpResponse
+from . import signals
+
+def index(request: HttpRequest) -> HttpResponse:
+    header = '''<!DOCTYPE html>
+<html>
+  <head>
+    <title>django-cas-ng example demo</title>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0">
+  </head>
+  <body>
+  <h1>Welcome to django-cas-ng demo</h1>'''
+
+    footer = '''<p>Related post:</p>
+    <ul>
+        <li><a href="https://djangocas.dev/blog/django-cas-ng-example-project/">Step by step to setup a django-cas-ng example project</a></li>
+    </ul>
+    <hr><p><a href="https://djangocas.dev/">Project homepage</a></p>
+  </body>
+</html>'''
+
+    if request.user.is_authenticated:
+        body = """
+        <p>You logged in as <strong>%s</strong>.</p>
+        <p><a href="/accounts/logout">Logout</a></p>
+         """ % request.user.username
+    else:
+        body = '<p><a href="/accounts/login">Login</a></p>'
+
+    return HttpResponse(header + body + footer)
+
+def ping(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('pong', content_type="text/plain")
