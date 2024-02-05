41485df14428bdb6873a5c81d4c18403e41487ad
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-02-05 23:12:26 +0100

    fastapi version (might actually use this one in the end)

--- src/app.py
+++ src/app.py
@@ -1,168 +1,192 @@
 # cas-sso-discord-bot
 
-#! USE QUARK ??
-#import quart.flask_patch
-#from quart import Quart, request, sessions, redirect, url_for, render_template
+from typing import Optional, List
 
-#! pip install flask[async] # https://flask.palletsprojects.com/en/3.0.x/async-await/
-from flask import Flask, request, session, redirect, url_for, render_template
-from threading import Thread
-#import asyncio # https://testdriven.io/blog/flask-async/
-# or use an WsgiToAsgi adapter: https://flask.palletsprojects.com/en/3.0.x/deploying/asgi/
+from cas import CASClient # https://github.com/Chise1/fastapi-cas-example
+from fastapi import FastAPI, Depends, Request
+from starlette.middleware.sessions import SessionMiddleware
+#from starlette.requests import Request
+#from starlette.responses import Response
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi_discord import DiscordOAuthClient, RateLimited, Unauthorized, User # https://github.com/Tert0/fastapi-discord
+from fastapi_discord.exceptions import ClientSessionNotInitialized
+from fastapi_discord.models import GuildPreview
+#* OR ? :
+#* from starlette_discord.client import DiscordOAuthClient # https://github.com/nwunderly/starlette-discord
+from os import getenv
 
-from cas import CASClient, CASError # https://github.com/python-cas/python-cas
-from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized # https://github.com/weibeu/Flask-Discord
-from os import getenv, environ
-
-#app = Quart(__name__)
-app = Flask(__name__)
-app.secret_key = getenv("APP_SECRET_KEY") # TODO: or generate at startup ?
-environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true" if getenv("DEV_ENV") else "false" # !! Only in development environment (no https).
+app = FastAPI(title=__name__)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+from fastapi.responses import RedirectResponse
 
 cas_client = CASClient(
-    version=getenv("CAS_VERSION"),#3, # v2 or v1 ?
-    service_url=getenv("CAS_SERVICE_URL"),
-    server_url=getenv("CAS_SERVER_URL")
+    version=getenv('CAS_VERSION'),
+    service_url=getenv('CAS_SERVICE_URL'),
+    server_url=getenv('CAS_SERVER_URL'),
 )
+app.add_middleware(SessionMiddleware, secret_key=getenv('SECRET'))
 
-app.config["DISCORD_CLIENT_ID"] = getenv("DISCORD_CLIENT_ID")           # Discord client ID.
-app.config["DISCORD_CLIENT_SECRET"] = getenv("DISCORD_CLIENT_SECRET")   # Discord client secret.
-app.config["DISCORD_REDIRECT_URI"] = getenv("DISCORD_REDIRECT_URI")     # URL to your callback endpoint.
-app.config["DISCORD_BOT_TOKEN"] = getenv("DISCORD_BOT_TOKEN")           # Required to access BOT resources.
+discord_auth = DiscordOAuthClient(
+    client_id=getenv('DISCORD_CLIENT_ID'),
+    client_secret=getenv('DISCORD_CLIENT_SECRET'),
+    #redirect_url=getenv('DISCORD_REDIRECT_URL'),
+    redirect_url=str(getenv("SITE_URL", "http://localhost:8000"))+"/discord-callback",
+    scopes=("identify")#, "guilds", "email") # scopes
+)
+DISCORD_TOKEN_URL = "https://discord.com/api/v10/oauth2/token" # ? https://github.com/Tert0/fastapi-discord/issues/96
 
-discordAuth = DiscordOAuth2Session(app)
-
-@app.route('/')
-def index():
-    #return redirect(url_for("login"))
-    return 'Hello! <a href="/login">CAS Login</a>'
-    #return render_template("index.html")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.route('/user')
-def user(method=['GET']):
-    if 'username' in session:
-        # Logged in with CAS
-        if not discordAuth.authorized:
-            # Not logged in with Discord
-            return 'Logged in as %s. <a href="/logout">Logout</a>' % session['username']
+@app.on_event("startup")
+async def on_startup():
+    await discord_auth.init()
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"hello": "world"}
+    )
+
+
+@app.get('/user', response_class=HTMLResponse)
+async def profile(request: Request):
+    print(request.session.get("user"))
+    user = request.session.get("user")
+    if user:
+        if await discord_auth.isAuthenticated(request.session['access_token']):
+            return templates.TemplateResponse(request=request, name="user_with_discord.html", context={"cas_username": user, "discord_id": discord_auth.id, "discord_username": discord_auth.username})
         else:
-            # Logged in with Discord
-            return render_template("user_with_discord.html", username=session['username'], discord_username=session['discord_username'])
-    # Not logged in with CAS
-    return 'Login required. <a href="/login">Login</a>', 403
+            return HTMLResponse('Logged in as %s. <a href="/logout">Logout</a>' % user['user'])
+    return HTMLResponse('Login required. <a href="/login">Login</a>', status_code=403)
 
 
-@app.route('/login')
-def login():
-    if 'username' in session:
+@app.get('/login')
+def login(
+    request: Request, next: Optional[str] = None,
+    ticket: Optional[str] = None):
+    if request.session.get("user", None):
         # Already logged in
-        return redirect(url_for('user'))
+        return RedirectResponse(request.url_for('profile'))
 
-    next = request.args.get('next')
-    ticket = request.args.get('ticket')
+    # next = request.args.get('next')
+    # ticket = request.args.get('ticket')
     if not ticket:
         # No ticket, the request come from end user, send to CAS login
         cas_login_url = cas_client.get_login_url()
-        app.logger.debug('CAS login URL: %s', cas_login_url)
-        return redirect(cas_login_url)
+        print('CAS login URL: %s', cas_login_url)
+        return RedirectResponse(cas_login_url)
 
     # There is a ticket, the request come from CAS as callback.
     # need call `verify_ticket()` to validate ticket and get user profile.
-    app.logger.debug('ticket: %s', ticket)
-    app.logger.debug('next: %s', next)
+    print('ticket: %s', ticket)
+    print('next: %s', next)
 
-    CASUser, attributes, pgtiou = cas_client.verify_ticket(ticket)
+    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
 
-    app.logger.debug(
-        'CAS verify ticket response: user: %s, attributes: %s, pgtiou: %s', CASUser, attributes, pgtiou)
+    print(
+        'CAS verify ticket response: user: %s, attributes: %s, pgtiou: %s',
+        user, attributes, pgtiou)
 
-    if not CASUser:
-        return 'Failed to verify ticket. <a href="/login">Login</a>'
+    if not user:
+        return HTMLResponse('Failed to verify ticket. <a href="/login">Login</a>')
     else:  # Login successfully, redirect according `next` query parameter.
-        session['username'] = CASUser
-        return redirect(next)
+        response = RedirectResponse(next)
+        request.session['user'] = dict(user=user)
+        return response
 
-@app.route('/discord-login')
-def discord_login():
-    if 'username' in session:
-        # Already logged in
-        # TODO: Discord OAuth login
-        return discordAuth.create_session()
-    else:
-        return redirect(url_for('login'))
-    
-@app.route("/discord-callback")
-def discord_callback():
-    data = discordAuth.callback()
 
-    app.logger.debug(data)
-    if getenv("DEBUG"):
-        print(data)
-
-    discordUser = discordAuth.fetch_user()
-    session['discord_username'] = discordUser.name
-
-    # User is logged in with CAS and Discord
-    link_accounts(session['username'], discordUser.id)
-    # Give role to Discord user
-    give_role(discordUser.id)
-
-    return redirect(url_for("user"))
-
-#? needed ?
-@app.errorhandler(Unauthorized)
-def redirect_unauthorized(e):
-    app.logger.info("Unauthorized request: %s", e)
-    print("Unauthorized request: ", e)
-    return redirect(url_for("login"))
-
-@app.route('/discord-logout')
-def discord_logout():
-    discordAuth.revoke() # Discord logout
-    return redirect(url_for("user"))
-
-@app.route('/logout')
-def logout():
-    discordAuth.revoke() # Discord logout
-
-    redirect_url = url_for('logout_callback', _external=True)
+@app.get('/logout')
+def logout(request: Request):
+    redirect_url = request.url_for('logout-callback')
     cas_logout_url = cas_client.get_logout_url(redirect_url)
-    app.logger.debug('CAS logout URL: %s', cas_logout_url)
+    print('CAS logout URL: %s', cas_logout_url)
+    return RedirectResponse(cas_logout_url)
 
-    return redirect(cas_logout_url) # CAS logout
 
-
-@app.route('/logout_callback')
-def logout_callback():
+@app.get('/logout-callback')
+def logout_callback(request: Request):
     # redirect from CAS logout request after CAS logout successfully
-    session.pop('username', None)
-    #return 'Logged out from CAS. <a href="/login">Login</a>'
-    return redirect(url_for('index'))
+    # response.delete_cookie('username')
+    request.session.pop("user", None)
+    return HTMLResponse('Logged out from CAS. <a href="/login">Login</a>')
 
-def link_accounts(cas_username, discord_id):
-    pass # TODO: add to database
 
-def give_role(discord_id):
+@app.get('/discord-login')
+async def discord_login(request: Request):
+    return await RedirectResponse(discord_auth.get_oauth_login_url(state="my_test_state")) # TODO: state https://discord.com/developers/docs/topics/oauth2#state-and-security
+    return await discord_auth.login(request) # ?
+    #return await RedirectResponse(discord_auth.oauth_login_url) # or discord_auth.get_oauth_login_url(state="my state")
 
-    pass # TODO: add to discord
 
-def run_flask():
-  app.run(host=getenv("FLASK_HOST"), port=getenv("FLASK_PORT"), debug=True if getenv("DEBUG") else False)
+@app.get('/discord-callback')
+async def discord_callback(request: Request, code: str, state: str):
+    token, refresh_token = await discord_auth.get_access_token(code)
+    request.session['discord_token'] = await discord_auth.get_token
+
+    assert state == "my_test_state" # compares state for security # TODO: state
+    
+    return await RedirectResponse(request.url_for('user'))
+    #try:
+    #    await discord_auth.callback(request)
+    #    return RedirectResponse(request.url_for('user'))
+    #except Unauthorized:
+    #    return JSONResponse({"error": "Unauthorized"}, status_code=401)
+    #except RateLimited:
+    #    return JSONResponse({"error": "RateLimited"}, status_code=429)
+    #except ClientSessionNotInitialized:
+    #    return JSONResponse({"error": "ClientSessionNotInitialized"}, status_code=500)
+
+
+#! needed ?
+@app.get(
+    "/authenticated",
+    dependencies=[Depends(discord_auth.requires_authorization)],
+    response_model=bool,
+)
+async def isAuthenticated(token: str = Depends(discord_auth.get_token)):
+    try:
+        auth = await discord_auth.isAuthenticated(token)
+        return auth
+    except Unauthorized:
+        return False
+
+
+@app.get('/discord-logout')
+async def discord_logout(request: Request):
+    pass
+
+
+@app.exception_handler(Unauthorized)
+async def unauthorized_error_handler(request: Request):
+    error = "Unauthorized"
+    return HTMLResponse(templates.TemplateResponse(request=request, name="401.html", context={"error": error}), status_code=401)
+    return JSONResponse({"error": "Unauthorized"}, status_code=401)
+
+
+@app.exception_handler(RateLimited)
+async def rate_limit_error_handler(request: Request, e: RateLimited):
+    return HTMLResponse(templates.TemplateResponse(request=request, name="429.html", context={"retry_after": e.retry_after}), status_code=429)
+    return JSONResponse(
+        {"error": "RateLimited", "retry": e.retry_after, "message": e.message},
+        status_code=429,
+    )
+
+
+@app.exception_handler(ClientSessionNotInitialized)
+async def client_session_error_handler(request: Request, e: ClientSessionNotInitialized):
+    print(e)
+    return HTMLResponse(templates.TemplateResponse(request=request, name="500.html", context={"error": e}), status_code=500)
+    return JSONResponse({"error": "Internal Error"}, status_code=500)
+
 
 if __name__ == '__main__':
+    import uvicorn
 
-    # generate app secret key if not set
-    if not app.secret_key:
-        from os import urandom
-        app.secret_key = urandom(24)
-    
-    #TODO: run webserver from different thread
-    flask_thread = Thread(target=run_flask)
-    flask_thread.start()
+    #TODO: start Discord bot
 
-    #app.run(debug=True)
-    #app.run(debug=True if getenv("DEBUG") else False)
+    uvicorn.run(app, port=getenv('PORT', 8000), host=getenv('HOST', '0.0.0.0'))
 
-    #TODO: run discord bot
-    # OR: run discord bot only when needed (on give_role() call)
