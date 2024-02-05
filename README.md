c6f92de3bac6c4bf20ba0e98a859501c02c12cd1
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-02-05 16:53:29 +0100

    initial flask app

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # cas-sso-discord-bot
 A Discord bot for authenticating users using CAS SSO.  
-Links their Discord account (ID) to their CAS credentials. Users are able to link and unlink their account, and only one Discord ID can be linked to a CAS user.
+Links their Discord account (ID) to their CAS credentials. Users are able to link and unlink their account, and only one Discord ID can be linked to a CAS user.  
 
 ### Currently at the idea stage
-Possibly replacing [our own previous bot **ULBDiscordBot**](https://github.com/bepolytech/ULBDiscordBot) and inspired by [Opensource-IIITH/Discord-CAS](https://github.com/Opensource-IIITH/Discord-CAS)
+Possibly replacing [our own previous bot **ULBDiscordBot**](https://github.com/bepolytech/ULBDiscordBot) and inspired by [Opensource-IIITH/Discord-CAS](https://github.com/Opensource-IIITH/Discord-CAS)  
 
-OR use:
+OR use:  
 
-- https://discord.com/developers/docs/tutorials/configuring-app-metadata-for-linked-roles
-- https://github.com/discord/linked-roles-sample
-- https://support.discord.com/hc/en-us/articles/10388356626711-Connections-Linked-Roles-Admins
+- https://discord.com/developers/docs/tutorials/configuring-app-metadata-for-linked-roles  
+- https://github.com/discord/linked-roles-sample  
+- https://support.discord.com/hc/en-us/articles/10388356626711-Connections-Linked-Roles-Admins  
 
-maybe with:
-- https://apereo.github.io/2019/02/19/cas61-as-oauth-authz-server/
+maybe with:  
+- https://apereo.github.io/2019/02/19/cas61-as-oauth-authz-server/  
+
+- https://github.com/weibeu/Flask-Discord  
+or  
+- https://github.com/treeben77/discord-oauth2.py  
 
 ## Acknowledgments
-- [**DocHub**](https://github.com/DocHub-ULB/DocHub): student-made ULB courses documents and summaries repository accessible via ULB's CAS auth. 
+- [**DocHub**](https://github.com/DocHub-ULB/DocHub): student-made ULB courses documents and summaries repository accessible via ULB's CAS auth.  
--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
-from flask import Flask, request, session, redirect, url_for
-from cas import CASClient, CASError # https://github.com/python-cas/python-cas
-from os import getenv
+# cas-sso-discord-bot
 
+#! USE QUARK ??
+#import quart.flask_patch
+#from quart import Quart, request, sessions, redirect, url_for, render_template
+
+from flask import Flask, request, session, redirect, url_for, render_template
+
+from cas import CASClient, CASError # https://github.com/python-cas/python-cas
+from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized # https://github.com/weibeu/Flask-Discord
+from os import getenv, environ
+
+#app = Quart(__name__)
 app = Flask(__name__)
 app.secret_key = getenv("APP_SECRET_KEY") # TODO: or generate at startup ?
+environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true" if getenv("DEV_ENV") else "false" # !! Only in development environment (no https).
 
 cas_client = CASClient(
     version=getenv("CAS_VERSION"),#3, # v2 or v1 ?
@@ -11,17 +21,31 @@ cas_client = CASClient(
     server_url=getenv("CAS_SERVER_URL")
 )
 
+app.config["DISCORD_CLIENT_ID"] = getenv("DISCORD_CLIENT_ID")           # Discord client ID.
+app.config["DISCORD_CLIENT_SECRET"] = getenv("DISCORD_CLIENT_SECRET")   # Discord client secret.
+app.config["DISCORD_REDIRECT_URI"] = getenv("DISCORD_REDIRECT_URI")     # URL to your callback endpoint.
+app.config["DISCORD_BOT_TOKEN"] = getenv("DISCORD_BOT_TOKEN")           # Required to access BOT resources.
+
+discordAuth = DiscordOAuth2Session(app)
 
 @app.route('/')
 def index():
-    #return redirect(url_for('login'))
+    #return redirect(url_for("login"))
     return 'Hello! <a href="/login">CAS Login</a>'
+    #return render_template("index.html")
 
 
 @app.route('/user')
 def user(method=['GET']):
     if 'username' in session:
-        return 'Logged in as %s. <a href="/logout">Logout</a>' % session['username']
+        # Logged in with CAS
+        if not discordAuth.authorized:
+            # Not logged in with Discord
+            return 'Logged in as %s. <a href="/logout">Logout</a>' % session['username']
+        else:
+            # Logged in with Discord
+            return render_template("user_with_discord.html", username=session['username'], discord_username=session['discord_username'])
+    # Not logged in with CAS
     return 'Login required. <a href="/login">Login</a>', 403
 
 
@@ -44,25 +68,64 @@ def login():
     app.logger.debug('ticket: %s', ticket)
     app.logger.debug('next: %s', next)
 
-    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
+    CASUser, attributes, pgtiou = cas_client.verify_ticket(ticket)
 
     app.logger.debug(
-        'CAS verify ticket response: user: %s, attributes: %s, pgtiou: %s', user, attributes, pgtiou)
+        'CAS verify ticket response: user: %s, attributes: %s, pgtiou: %s', CASUser, attributes, pgtiou)
 
-    if not user:
+    if not CASUser:
         return 'Failed to verify ticket. <a href="/login">Login</a>'
     else:  # Login successfully, redirect according `next` query parameter.
-        session['username'] = user
+        session['username'] = CASUser
         return redirect(next)
 
+@app.route('/discord-login')
+def discord_login():
+    if 'username' in session:
+        # Already logged in
+        # TODO: Discord OAuth login
+        return discordAuth.create_session()
+    else:
+        return redirect(url_for('login'))
+    
+@app.route("/discord-callback")
+def discord_callback():
+    data = discordAuth.callback()
+
+    app.logger.debug(data)
+    if getenv("DEBUG"):
+        print(data)
+
+    discordUser = discordAuth.fetch_user()
+    session['discord_username'] = discordUser.name
+
+    # User is logged in with CAS and Discord
+    link_accounts(session['username'], discordUser.id)
+    # Give role to Discord user
+    give_role(discordUser.id)
+
+    return redirect(url_for("user"))
+
+#? needed ?
+@app.errorhandler(Unauthorized)
+def redirect_unauthorized(e):
+    print("Unauthorized request: ", e)
+    return redirect(url_for("login"))
+
+@app.route('/discord-logout')
+def discord_logout():
+    discordAuth.revoke() # Discord logout
+    return redirect(url_for("user"))
 
 @app.route('/logout')
 def logout():
+    discordAuth.revoke() # Discord logout
+
     redirect_url = url_for('logout_callback', _external=True)
     cas_logout_url = cas_client.get_logout_url(redirect_url)
     app.logger.debug('CAS logout URL: %s', cas_logout_url)
 
-    return redirect(cas_logout_url)
+    return redirect(cas_logout_url) # CAS logout
 
 
 @app.route('/logout_callback')
@@ -71,3 +134,26 @@ def logout_callback():
     session.pop('username', None)
     #return 'Logged out from CAS. <a href="/login">Login</a>'
     return redirect(url_for('index'))
+
+def link_accounts(cas_username, discord_id):
+    pass # TODO: add to database
+
+def give_role(discord_id):
+
+    pass # TODO: add to discord
+
+
+
+if __name__ == '__main__':
+
+    # generate app secret key if not set
+    if not app.secret_key:
+        from os import urandom
+        app.secret_key = urandom(24)
+
+    #TODO: run discord bot
+    
+    #TODO: run webserver from different thread
+
+    #app.run(debug=True)
+    app.run(debug=True if getenv("DEBUG") else False)
