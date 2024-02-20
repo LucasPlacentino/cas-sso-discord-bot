db84c0e8f29cea44e1be914977eed75b3998e985
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2024-02-20 15:48:33 +0100

    DocHub's custom CAS auth

--- src/CASAuth.py
+++ src/CASAuth.py
@@ -2,28 +2,151 @@
 
 import os
 from dotenv import load_dotenv
-import requests
+#import requests
+from httpx import AsyncClient
 from furl import furl
+import xml.etree.ElementTree as ET
+
+from models import User
+from app import app
+from fastapi import Request
 
 load_dotenv()
 
-# TODO:
+# TODO: all this for custom CAS authentication implementation
 
+"""
+Code originally from DocHub: https://github.com/DocHub-ULB/DocHub
+"""
 
-class CASAuth
-  CAS_ENDPOINT = os.getenv("CAS_ENDPOINT")
-  LOGIN_METHOD = "cas"
+class CASAuth:
+    CAS_ENDPOINT = os.getenv("CAS_ENDPOINT")
+    LOGIN_METHOD = "cas"
     XML_NAMESPACES = {
-        "cas": "http://www.yale.edu/tp/cas",
-    }
-  CAS_TICKET_URL_PATH = os.getenv("CAS_TICKET_URL_PATH") # "/proxyValidate"
+            "cas": "http://www.yale.edu/tp/cas",
+        }
+    CAS_TICKET_URL_PATH = os.getenv("CAS_TICKET_URL_PATH") # "/proxyValidate"
 
-  def getUser(self):
+    async def getUser(self, request: Request, ticket=None):
+        if not ticket:
+            return None
+        
+        # Craft request to the CAS provider
+        cas_ticket_url = furl(self.CAS_ENDPOINT)
+        cas_ticket_url.path = "/proxyValidate" # os.getenv("CAS_TICKET_URL_PATH")
+        cas_ticket_url.args["ticket"] = ticket
+        cas_ticket_url.args["service"] = self.get_service_url()
+        
+        # Send the request
+        #resp = requests.get(cas_ticket_url.url)
+        async with AsyncClient(app=app) as ac:
+            response = await ac.get(cas_ticket_url.url, cert=os.getenv("CAS_CERT_PATH")) #! cert needed ?
+        
+        if response.status_code != 200:
+            #return None
+            raise CasRequestError(response)
+        
+        user_dict = self._parse_response(response.text)
+
+        # Get or create a user from the parsed user_dict
+        #TODO:
+        try:
+            user = User.objects.get(netid=user_dict["netid"])
+        except User.DoesNotExist:
+            user = User.objects.create_user(
+                netid=user_dict["netid"],
+                email=user_dict["email"],
+                first_name=user_dict["first_name"],
+                last_name=user_dict["last_name"],
+                register_method=self.LOGIN_METHOD,
+            )
+        user.last_login_method = self.LOGIN_METHOD
+        user.save()
+
+        return user
+
+    # TODO: adapt
+    def _parse_response(self, xml):
+        # Try to parse the response from the CAS provider
+        try:
+            tree = ET.fromstring(xml)
+        except ET.ParseError as e:
+            raise CasParseError("INVALID_XML", xml) from e
+
+        success = tree.find(
+            "./cas:authenticationSuccess", namespaces=self.XML_NAMESPACES
+        )
+        if not success:
+            failure = tree.find(
+                "./cas:authenticationFailure", namespaces=self.XML_NAMESPACES
+            )
+            if failure is not None:
+                raise CasRejectError(failure.attrib.get("code"), failure.text)
+            else:
+                raise CasParseError("UNKNOWN_STRUCTURE", xml)
+
+        netid_node = success.find("cas:user", namespaces=self.XML_NAMESPACES)
+        if netid_node is not None:
+            netid = netid_node.text
+        else:
+            raise CasParseError("UNKNOWN_STRUCTURE", xml)
+
+        email_node = success.find(
+            "./cas:attributes/cas:mail", namespaces=self.XML_NAMESPACES
+        )
+        if email_node is not None:
+            email = email_node.text
+        else:
+            email = f"{netid}@ulb.ac.be"
+
+        first_name_node = success.find(
+            "./cas:attributes/cas:givenName", namespaces=self.XML_NAMESPACES
+        )
+        last_name_node = success.find(
+            "./cas:attributes/cas:sn", namespaces=self.XML_NAMESPACES
+        )
+
+        return {
+            "netid": netid, # userid ?
+            "email": email,
+            "first_name": first_name_node.text
+            if first_name_node is not None
+            else netid,
+            "last_name": last_name_node.text if last_name_node is not None else netid,
+        }
+
+
+    @classmethod
+    def get_login_url(cls):
+        url = furl(cls.CAS_ENDPOINT)
+        url.path = "/login" # os.getenv("CAS_LOGIN_PATH")
+        url.args["service"] = cls.get_service_url()
+
+        return url.url
+
+    @classmethod
+    def get_service_url(cls, request: Request):
+        url = furl(os.getenv("CAS_SERVICE_BASE_URL")) #settings.BASE_URL)
+        url.path = request.url_for("auth-ulb") # os.getenv("CAS_SERVICE_PATH")
+        return url.url
+
+    def authenticate(self, request, ticket=None):
+        if not ticket:
+            return None
+        pass
+
+class CasError(Exception):
     pass
 
-  def authenticate(self, request, ticket=None):
-    if not ticket:
-      return None
+
+class CasRequestError(CasError):
     pass
 
+
+class CasParseError(CasError):
+    pass
+
+
+class CasRejectError(CasError):
+    pass
 
