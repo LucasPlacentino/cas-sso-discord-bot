834c4cf5dd13f4ec7441cfe9a1b17d8b3e4eec11
LucasPlacentino <23436953+LucasPlacentino@users.noreply.github.com>
2024-10-01 21:37:32 +0200

    change lang files locations and handling

---
+++ src/lang.py
@@ -0,0 +1,36 @@
+from os import getenv, listdir, path
+import json
+import logging
+
+DEFAULT_LANG = getenv("DEFAULT_LANG", "en")
+
+languages = {}
+lang_list = [] # redundant with languages.keys()
+
+locales_path: str = path.join(path.dirname(__file__),"../locales/")
+
+#for file in listdir(path.join(path.dirname(__file__))):
+for file in listdir(locales_path):
+    if not file.endswith(".json"): #only get json files
+        continue
+    lang_code = file.split(".")[0] # get filename without extension
+    if len(lang_code) == 5: # handle cases like "en_US.json"
+        pre = file.split("_")[0]
+        if pre is None or len(pre) != 2:
+            continue
+        lang_code = pre
+    elif len(lang_code) != 2: # filename should be a 2-letter language code like "en.json"
+        continue
+    print(f"Found locale file: {locales_path}{file} => lang_code: {lang_code}")
+    try:
+        with open(path.join(locales_path, file), "r", encoding="utf-8") as lang_file:
+            languages[lang_code] = json.load(lang_file) # load json file as lang dict
+            lang_list.append(lang_code)
+        print(f"Successfully loaded {lang_code} locale file")
+    except Exception as e:
+        print(f"Failed to load {lang_code} locale file: {e}")
+
+def lang_str(string: str, user_lang: str) -> str:
+    if user_lang not in languages.keys():
+        user_lang = DEFAULT_LANG
+    return languages[user_lang].get(string, languages[DEFAULT_LANG].get(string, string))
