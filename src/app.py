811170914256a859e63bdfd16b04e907ed45323c
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2025-05-13 11:53:22 +0200

    need to change CASClient version ?

--- src/app.py
+++ src/app.py
@@ -177,7 +177,7 @@ app = App(
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 # CAS Client
-cas_client = CASClient( # Use CASClientV2 or V3 ? for proxy_callback url (for pgt from pgtiou)
+cas_client = CASClient( # Use CASClientV2 or V3 ? for proxy_callback url (for pgt from pgtiou) see docs: https://djangocas.dev/docs/latest/_modules/cas.html#CASClient
     version=getenv('CAS_VERSION', 1),
     #service_url=getenv('CAS_SERVICE_URL', "http://localhost:8000/login"),
     service_url=(str(getenv('SITE_URL', "http://localhost:8000")) if not DEV_ENV else "http://localhost:8000")+"/login",
